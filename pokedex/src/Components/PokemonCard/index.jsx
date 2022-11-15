6fecac4bbf70ff26c73971f09b0fa4e3082353a7
Gleison Almeida <gleisonalmeida.f2@gmail.com>
2022-11-15 15:35:12 -0300

    Primeira vesão da pokedex

---
+++ pokedex/src/Components/PokemonCard/index.jsx
@@ -0,0 +1,52 @@
+import * as React from 'react';
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import Typography from '@mui/material/Typography';
+import { Button, CardActionArea, CardActions } from '@mui/material';
+
+export default function PokemonCard( {name, image, types} ) {
+
+  const typeHandler = () => {
+    if(types[1]){
+      return types[0].type.name+" | "+types[1].type.name;
+    }
+    return types[0].type.name
+
+  }
+
+
+  return (
+    <Card sx={{ maxWidth: 345 }}>
+      <CardActionArea>
+        <CardMedia
+          component="img"
+          height="250"
+          image={image}
+          alt="pokemon"
+        />
+        <CardContent>
+          <Typography gutterBottom variant="h5" component="div">
+            {name}
+          </Typography>
+          <Typography gutterBottom variant="h6" component="div">
+            {typeHandler()}
+          </Typography>
+
+          {/* <Typography variant="body2" color="text.secondary">
+            Lizards are a widespread group of squamate reptiles, with over 6,000
+            species, ranging across all continents except Antarctica
+          </Typography> */}
+
+        </CardContent>
+      </CardActionArea>
+
+      {/* <CardActions>
+        <Button size="small" color="primary">
+          Share
+        </Button>
+      </CardActions> */}
+
+    </Card>
+  );
+}
